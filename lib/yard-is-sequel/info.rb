06d1ac1dac4ca57a090e1333a81e54dc3cc7bfbe
Иван Шихалев <shikhalev@gmail.com>
2026-02-04 18:57:48 +0500

    Ср 04 фев 2026 18:57:48 +05

---
+++ lib/yard-is-sequel/info.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module IS; end
+module IS::YARD; end
+module IS::YARD::Sequel; end
+
+module IS::YARD::Sequel::Info
+
+  NAME     = 'yard-is-sequel'
+  VERSION  = '0.8.0'
+  SUMMARY  = 'YARD-plugin for Sequel-models documenting'
+  AUTHOR   = 'Ivan Shikhalev'
+  LICENSE  = 'GPL-3.0-or-later'
+  HOMEPAGE = 'https://github.com/inat-get/yard-is-sequel'
+
+end
